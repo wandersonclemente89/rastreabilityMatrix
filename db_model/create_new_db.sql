9850491beeb1122a891f0c7b803e5eac22575eef
wanderson clemente <wanderson.clemente@gmail.com>
2017-08-03 10:08:03 -0300

    Updating sql file to be suitable to  gerritMetric system

--- db_model/create_new_db.sql
+++ db_model/create_new_db.sql
@@ -39,6 +39,9 @@ DROP TABLE IF EXISTS `rm_db`.`sprint` ;
 CREATE TABLE IF NOT EXISTS `rm_db`.`sprint` (
   `ID` INT(11) NOT NULL AUTO_INCREMENT,
   `NAME` VARCHAR(45) NOT NULL,
+  `START_DATE` DATE NOT NULL,
+  `END_DATE` DATE NOT NULL,
+  `LOCK_DATE` DATE NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE INDEX `NAME_UNIQUE` (`NAME` ASC))
 ENGINE = InnoDB
@@ -61,12 +64,12 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`buc_has_sprint` (
     FOREIGN KEY (`BUC_ID`)
     REFERENCES `rm_db`.`buc` (`ID`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE,
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_BUC_has_SPRINT_SPRINT1`
     FOREIGN KEY (`SPRINT_ID`)
     REFERENCES `rm_db`.`sprint` (`ID`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -144,6 +147,7 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`employees` (
   `NAME` VARCHAR(80) NOT NULL,
   `ROLE` VARCHAR(45) NOT NULL,
   `TEAM_ID` INT(11) NULL DEFAULT NULL,
+  `EMAIL` VARCHAR(100) NULL,
   PRIMARY KEY (`SIGNUM`),
   UNIQUE INDEX `SIGNUM_UNIQUE` (`SIGNUM` ASC),
   INDEX `fk_EMPLOYEES_TEAM_idx` (`TEAM_ID` ASC),
@@ -170,13 +174,13 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`business_requirements_has_employees` (
   CONSTRAINT `fk_BUSINESS_REQUIREMENTS_has_EMPLOYEES_BUSINESS_REQUIREMENTS1`
     FOREIGN KEY (`BUSINESS_REQUIREMENTS_ID`)
     REFERENCES `rm_db`.`business_requirements` (`ID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_BUSINESS_REQUIREMENTS_has_EMPLOYEES_EMPLOYEES1`
     FOREIGN KEY (`EMPLOYEES_SIGNUM`)
     REFERENCES `rm_db`.`employees` (`SIGNUM`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -196,12 +200,12 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`team_has_buc` (
     FOREIGN KEY (`BUC_ID`)
     REFERENCES `rm_db`.`buc` (`ID`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE,
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_TEAM_has_BUC_TEAM1`
     FOREIGN KEY (`TEAM_ID`)
     REFERENCES `rm_db`.`team` (`ID`)
     ON DELETE CASCADE
-    ON UPDATE CASCADE)
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -225,8 +229,8 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`technical_requirements` (
   CONSTRAINT `fk_TECHNICAL_REQUIREMENTS_BUSINESS_REQUIREMENTS1`
     FOREIGN KEY (`BUSINESS_REQUIREMENTS_ID`)
     REFERENCES `rm_db`.`business_requirements` (`ID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_TECHNICAL_REQUIREMENTS_STATUS1`
     FOREIGN KEY (`STATUS_ID`)
     REFERENCES `rm_db`.`status` (`ID`)
@@ -258,14 +262,19 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`test_cases` (
   `ID` INT(11) NOT NULL AUTO_INCREMENT,
   `NAME` VARCHAR(45) NOT NULL,
   `FULL_PATH` VARCHAR(200) NOT NULL,
+  `EMPLOYEES_SIGNUM` VARCHAR(7) NOT NULL,
   `STATUS_ID` INT(11) NOT NULL,
   `TEST_TYPE_ID` INT(11) NOT NULL,
-  `team_ID` INT(11) NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE INDEX `ID_UNIQUE` (`ID` ASC),
+  INDEX `fk_TEST_CASES_EMPLOYEES1_idx` (`EMPLOYEES_SIGNUM` ASC),
   INDEX `fk_TEST_CASES_STATUS1_idx` (`STATUS_ID` ASC),
   INDEX `fk_TEST_CASES_TEST_TYPE1_idx` (`TEST_TYPE_ID` ASC),
-  INDEX `fk_test_cases_team1_idx` (`team_ID` ASC),
+  CONSTRAINT `fk_TEST_CASES_EMPLOYEES1`
+    FOREIGN KEY (`EMPLOYEES_SIGNUM`)
+    REFERENCES `rm_db`.`employees` (`SIGNUM`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_TEST_CASES_STATUS1`
     FOREIGN KEY (`STATUS_ID`)
     REFERENCES `rm_db`.`status` (`ID`)
@@ -274,11 +283,6 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`test_cases` (
   CONSTRAINT `fk_TEST_CASES_TEST_TYPE1`
     FOREIGN KEY (`TEST_TYPE_ID`)
     REFERENCES `rm_db`.`test_type` (`ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_test_cases_team1`
-    FOREIGN KEY (`team_ID`)
-    REFERENCES `rm_db`.`team` (`ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -299,42 +303,37 @@ CREATE TABLE IF NOT EXISTS `rm_db`.`technical_requirements_has_test_cases` (
   CONSTRAINT `fk_TECHNICAL_REQUIREMENTS_has_TEST_CASES_TECHNICAL_REQUIREMEN1`
     FOREIGN KEY (`TECHNICAL_REQUIREMENTS_ID`)
     REFERENCES `rm_db`.`technical_requirements` (`ID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_TECHNICAL_REQUIREMENTS_has_TEST_CASES_TEST_CASES1`
     FOREIGN KEY (`TEST_CASES_ID`)
     REFERENCES `rm_db`.`test_cases` (`ID`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `rm_db`.`change_report`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `rm_db`.`change_report` ;
+
+CREATE TABLE IF NOT EXISTS `rm_db`.`change_report` (
+  `ID` INT NOT NULL,
+  `TEAM` VARCHAR(50) NOT NULL,
+  `SPRINT` VARCHAR(45) NOT NULL,
+  `CHANGE_ID` VARCHAR(45) NOT NULL,
+  `CREATED_ON` DATE NOT NULL,
+  `PATCHSETS_REWORK` INT NULL,
+  `NUM_COMMENTS` INT NULL,
+  `TIME_TO_MERGE` INT NOT NULL,
+  `LOCK_DATE` DATE NOT NULL,
+  `IS_DELIVERED_LATE` TINYINT(1) NOT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
--- -----------------------------------------------------
--- Data for table `rm_db`.`status`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `rm_db`;
-INSERT INTO `rm_db`.`status` (`ID`, `NAME`) VALUES (1, 'Enabled');
-INSERT INTO `rm_db`.`status` (`ID`, `NAME`) VALUES (2, 'Disabled');
-INSERT INTO `rm_db`.`status` (`ID`, `NAME`) VALUES (3, 'Postponed');
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `rm_db`.`test_type`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `rm_db`;
-INSERT INTO `rm_db`.`test_type` (`ID`, `NAME`) VALUES (1, 'Functional');
-INSERT INTO `rm_db`.`test_type` (`ID`, `NAME`) VALUES (2, 'Unit');
-INSERT INTO `rm_db`.`test_type` (`ID`, `NAME`) VALUES (3, 'E2E');
-INSERT INTO `rm_db`.`test_type` (`ID`, `NAME`) VALUES (4, 'Performance');
-
-COMMIT;
-
